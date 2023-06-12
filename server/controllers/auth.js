ffe46842d8a029ce989e09a27a416ea01738513c
nlagdhir <nlagdhir@gmail.com>
2023-06-12 21:25:51 +0600

    'login'

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -51,49 +51,7 @@ const register = (req, res) => {
   });
 };
 
-const login = (req, res) => {
-  const errors = validationResult(req);
 
-  if (!errors.isEmpty()) {
-    return res.status(422).json({ errors: errors.array() });
-  }
-
-  // CHECK USER EXISTS
-
-  const q = "SELECT * FROM registration WHERE email = ?";
-  try {
-    db.query(q, [req.body.email], (error, data) => {
-      if (error) return res.json(error);
-
-      if (data.length === 0)
-        return res.status(404).json({ status: 0, message: "User not found!" });
-
-      // CHECK PASSWORD
-      const salt = bcrypt.genSaltSync(10);
-      const hashedPassword = bcrypt.hashSync(req.body.password, salt);
-      const isPasswordCorrect = bcrypt.compareSync(
-        req.body.password,
-        data[0].password
-      );
-      9;
-      if (!isPasswordCorrect) {
-        return res.status(400).json({ status: 0, message: "Wrong Password!" });
-      }
-
-      const token = jwt.sign({ id: data[0].id }, "jwtkey");
-      const { password, ...rest } = data[0];
-
-      res
-        .cookie("access_token", token, {
-          httpOnly: true,
-        })
-        .status(200)
-        .json({ status: 1, data: rest, message: "Login successful!" });
-    });
-  } catch (error) {
-    console.log(error);
-  }
-};
 
 // There is very simple solution for this. Follow the following steps to send emails from your gmail using node (nodemailer)
 
