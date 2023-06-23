048c96e6e0771b9dc65a41fa5185abc878e6fa32
nlagdhir <nlagdhir@gmail.com>
2023-06-23 22:03:15 +0600

    All party table responsive

--- frontend/src/components/Party/AllParties.js
+++ frontend/src/components/Party/AllParties.js
@@ -6,7 +6,7 @@ import { RiDeleteBinLine } from "react-icons/ri";
 function AllParties({ handlePartyEdit, data, partyData }) {
   return (
     <div className="md:w-3/5">
-      <div className="shadow-lg bg-white rounded border-t-4 border-blue-400 overflow-hidden ml-2">
+      <div className="shadow-lg bg-white rounded border-t-4 border-blue-400 overflow-hidden md:ml-2">
         <h2 className="w-full py-3 border-b-2 shadow-md mb-6 font-bold text-xl px-4">
           All Parties
         </h2>
@@ -33,67 +33,69 @@ function AllParties({ handlePartyEdit, data, partyData }) {
             />
           </div>
         </div>
-        <table className="w-full table-fixed">
-          <thead>
-            <tr className="bg-gray-100">
-              <th className="w-1/4 py-4 px-3 text-left text-gray-600  font-semibold uppercase">
-                Name
-              </th>
-              <th className="w-1/4 py-4 px-3 text-left text-gray-600  font-semibold uppercase">
-                Contact
-              </th>
-              <th className="w-1/4 py-4 px-3 text-left text-gray-600  font-semibold uppercase">
-                Email
-              </th>
-              <th className="w-1/4 py-4 px-3 text-left text-gray-600  font-semibold uppercase">
-                Mobile
-              </th>
-              <th className="w-1/4 py-4 px-3 text-left text-gray-600  font-semibold uppercase">
-                Action
-              </th>
-            </tr>
-          </thead>
-          <tbody className="bg-white">
-            {partyData
-              ?.slice()
-              .reverse()
-              .map((d) => (
-                <tr key={d._id}>
-                  <td className="py-4 px-3 border-b border-gray-200">
-                    {d.name}
-                  </td>
-                  <td className="py-4 px-3 border-b border-gray-200 truncate">
-                    {d.contactPerson}
-                  </td>
-                  <td className="py-4 px-3 border-b border-gray-200 truncate">
-                    {d.email}
-                  </td>
-                  <td className="py-4 px-3 border-b border-gray-200">
-                    {" "}
-                    {d.mobile}
-                  </td>
-                  <td className="flex items-center py-5 px-2 border-b border-gray-200">
-                    <FiEye
-                      size={20}
-                      className="mr-4 cursor-pointer text-green-500"
-                    />
-                    <FiEdit
-                      onClick={() => handlePartyEdit(d._id)}
-                      size={20}
-                      className="mr-4 cursor-pointer text-red-500"
-                    />
-                    <RiDeleteBinLine
-                      size={20}
-                      className="mr-3 cursor-pointer text-blue-500"
-                    />
-                    {/* <span className="bg-green-500 text-white py-1 px-2 rounded-full text-xs">
+        <div className="overflow-x-auto">
+          <table className="table">
+            <thead>
+              <tr className="bg-gray-100">
+                <th className="w-1/5 md:w-1/4 py-4 px-3 text-sm text-left text-gray-600  font-semibold uppercase">
+                  Name
+                </th>
+                <th className="w-1/5 md:w-1/4 py-4 px-3 text-sm text-left text-gray-600  font-semibold uppercase">
+                  Contact
+                </th>
+                <th className="w-1/5 md:w-1/4 py-4 px-3 text-sm text-left text-gray-600  font-semibold uppercase">
+                  Email
+                </th>
+                <th className="w-1/5 md:w-1/4 py-4 px-3 text-sm text-left text-gray-600  font-semibold uppercase">
+                  Mobile
+                </th>
+                <th className="w-1/5 md:w-1/4 py-4 px-3 text-sm text-left text-gray-600  font-semibold uppercase">
+                  Action
+                </th>
+              </tr>
+            </thead>
+            <tbody className="bg-white">
+              {partyData
+                ?.slice()
+                .reverse()
+                .map((d) => (
+                  <tr key={d._id}>
+                    <td className="py-4 text-sm px-3 border-b border-gray-200">
+                      {d.name}
+                    </td>
+                    <td className="py-4 px-3 text-sm border-b border-gray-200 truncate">
+                      {d.contactPerson}
+                    </td>
+                    <td className="py-4 px-3 text-sm border-b border-gray-200 truncate">
+                      {d.email}
+                    </td>
+                    <td className="py-4 px-3 border-b text-xs border-gray-200">
+                      {" "}
+                      {d.mobile}
+                    </td>
+                    <td className="flex items-center  py-5 px-2 border-b border-gray-200">
+                      <FiEye
+                        size={20}
+                        className="mr-4 cursor-pointer text-green-500"
+                      />
+                      <FiEdit
+                        onClick={() => handlePartyEdit(d._id)}
+                        size={20}
+                        className="mr-4 cursor-pointer text-red-500"
+                      />
+                      <RiDeleteBinLine
+                        size={20}
+                        className="mr-3 cursor-pointer text-blue-500"
+                      />
+                      {/* <span className="bg-green-500 text-white py-1 px-2 rounded-full text-xs">
                 Active
               </span> */}
-                  </td>
-                </tr>
-              ))}
-          </tbody>
-        </table>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
